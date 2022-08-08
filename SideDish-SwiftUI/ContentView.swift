0e3b2c24e2cd7812d59f3554806dfb8d48f7c7f3
rising-jun <jun36817@naver.com>
2022-08-08 15:19:58 +0900

    Initial Commit

---
+++ SideDish-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideDish-SwiftUI
+//
+//  Created by 김동준 on 2022/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideDish-SwiftUI/SideDish_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideDish_SwiftUIApp.swift
+//  SideDish-SwiftUI
+//
+//  Created by 김동준 on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct SideDish_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
