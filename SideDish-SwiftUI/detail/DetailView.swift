08d292bd05a3a68195df84ea990ba4c9b656d6c0
rising-jun <jun36817@naver.com>
2022-08-16 17:03:02 +0900

    [Feature] : 상세화면 구현 완료.

--- SideDish-SwiftUI/detail/DetailView.swift
+++ SideDish-SwiftUI/detail/DetailView.swift
@@ -11,16 +11,18 @@ struct DetailView: View {
     init(id: String, menu: Menu) {
         viewModel = DetailViewModel(id: id)
         self.menu = menu
+        self.count = 1
     }
     @ObservedObject var viewModel: DetailViewModel
     @State var menu: Menu
+    @State var count: Int
     var body: some View {
         GeometryReader { p in
-            ScrollView{
+            ScrollView {
                 ZStack {
                     VStack(alignment: .leading) {
                         if let menuDetail = viewModel.menuDetail {
-                            DetailThumbImages(menuDetail: menuDetail)
+                            DetailThumbImages(thumbViewModel: ThumbViewModel(imageURLs: menuDetail.thumb_images))
                                 .frame(width: p.size.width, height: p.size.width, alignment: .top)
                         }
                         Text(menu.title)
@@ -34,11 +36,11 @@ struct DetailView: View {
                             .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
                         
                         HStack {
-                            Text(menu.n_price ?? "")
+                            Text(menu.s_price ?? "")
                                 .lineLimit(1)
                                 .font(.system(size: 18, weight: .medium, design: .default))
                                 .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 0))
-                            Text("~\(menu.s_price ?? "")~")
+                            Text("~\(menu.n_price ?? "")~")
                                 .lineLimit(1)
                                 .font(.system(size: 18, weight: .medium, design: .default))
                                 .foregroundColor(.gray)
@@ -88,13 +90,41 @@ struct DetailView: View {
                                     .padding(EdgeInsets(top: 15, leading: 15, bottom: 5, trailing: 20))
                             }.padding(.bottom)
                             Divider()
+                            
+                            ZStack {
+                                HStack {
+                                    Text("수량")
+                                        .font(.system(size: 15, weight: .medium, design: .default))
+                                        .foregroundColor(.gray)
+                                        .padding(EdgeInsets(top: 15, leading: 15, bottom: 5, trailing: 5))
+                                    Spacer()
+                                    Text("\(viewModel.count)")
+                                        .font(.system(size: 15, weight: .medium, design: .default))
+                                        .foregroundColor(.black)
+                                        .padding(EdgeInsets(top: 15, leading: 15, bottom: 5, trailing: 5))
+                                    plusMinusButton(buttonTapped: viewModel.buttonTapped)
+                                        .padding()
+                                }
+                            }
+                            Divider()
+                            
+                            HStack(alignment: .center, spacing: 10, content: {
+                                Spacer()
+                                Text("총 주문 금액")
+                                    .font(.system(size: 18, weight: .medium, design: .default))
+                                    .foregroundColor(.gray)
+                                    .padding()
+                                
+                                Text(viewModel.price)
+                                    .font(.system(size: 22, weight: .medium, design: .default))
+                                    .foregroundColor(.black)
+                                    .padding()
+                            })
                         }
-                        
                     }
                 }
             }
         }
-        
         .navigationTitle(menu.title)
         .navigationBarTitleDisplayMode(.inline)
         .onAppear {
@@ -103,13 +133,32 @@ struct DetailView: View {
     }
 }
 
-struct DetailThumbImages: View {
-    @ObservedObject var thumbViewModel: ThumbViewModel
-    
-    init(menuDetail: MenuDetail) {
-        thumbViewModel = ThumbViewModel(imageURLs: menuDetail.thumb_images)
-        UIPageControl.appearance().currentPageIndicatorTintColor = .blue
+struct plusMinusButton: View {
+    var buttonTapped: ((ButtonKind) -> ())
+    init(buttonTapped: @escaping (ButtonKind) -> ()) {
+        self.buttonTapped = buttonTapped
     }
+    var body: some View {
+        ZStack {
+            HStack {
+                Button("-") {
+                    buttonTapped(.minus)
+                }.frame(width: 50, height: 35, alignment: .center).background(Color.gray)
+                    .foregroundColor(.black)
+                    .padding(.trailing, 2)
+                Button("+") {
+                    buttonTapped(.plus)
+                }.frame(width: 50, height: 35, alignment: .center).background(Color.gray)
+                    .foregroundColor(.black)
+                    .padding(.trailing, 5)
+            }
+        }
+    }
+}
+
+struct DetailThumbImages: View {
+    @StateObject var thumbViewModel: ThumbViewModel
+    
     var body: some View {
         ZStack {
             if thumbViewModel.imageData.count > 0 {
@@ -123,7 +172,13 @@ struct DetailThumbImages: View {
                 } .tabViewStyle(PageTabViewStyle())
             }
         }.onAppear {
+            UIPageControl.appearance().currentPageIndicatorTintColor = .blue
             thumbViewModel.fetchThumbImages()
         }
     }
+}
+
+enum ButtonKind {
+    case plus
+    case minus
 }
--- SideDish-SwiftUI/detail/DetailViewModel.swift
+++ SideDish-SwiftUI/detail/DetailViewModel.swift
@@ -9,13 +9,40 @@ import Foundation
 import Combine
 
 final class DetailViewModel: ObservableObject {
-    @Published private(set) var menuDetail: MenuDetail?
+    @Published private(set) var menuDetail: MenuDetail? {
+        didSet {
+            price = menuDetail?.prices.last ?? ""
+        }
+    }
     @Published private(set) var thumbImages: [Data] = []
+    @Published private(set) var count: Int = 1 {
+        willSet(value) {
+            guard let priceString = menuDetail?.prices.last else { return }
+            guard var intPrice = Int(priceString.components(separatedBy: ["원",","]).joined()) else { return }
+            intPrice = intPrice * value
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal
+            guard var formatted = numberFormatter.string(from: NSNumber(value: intPrice)) else { return }
+            formatted += "원"
+            self.price = formatted
+        }
+    }
+    @Published private(set) var price: String = ""
+    
     private let id: String
     init(id: String) {
         self.id = id
     }
     
+    lazy var buttonTapped: ((ButtonKind) -> ()) = { [weak self] kind in
+        guard let self = self else { return }
+        switch kind {
+        case .plus:
+            self.count += 1
+        case .minus:
+            self.count -= 1
+        }
+    }
     func fetchOnbanDetail() {
         guard let url = URL(string: "https://api.codesquad.kr/onban/detail/\(id)") else { return }
         let meuCancellabel = URLSession.shared.dataTaskPublisher(for: url)
@@ -26,9 +53,5 @@ final class DetailViewModel: ObservableObject {
             .replaceError(with: nil)
             .receive(on: DispatchQueue.main)
             .assign(to: \.menuDetail, on: self)
-    }
-    
-    func fetchThumbImages() {
-        
     }
 }
